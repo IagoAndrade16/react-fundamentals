2d899e72e9958407536f8668eb3e6daeeee23c55
Iago Alexandre <iagoaap@outlook.com>
2023-01-29 09:57:23 -0300

    estrutura da aplicacao - CSS global

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+import "./global.css"
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post 
       author="Diego Fernandes" 
       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus cumque commodi iste cupiditate aperiam quae blanditiis id, ratione harum, delectus, hic nobis. Deleniti illo fugiat ab obcaecati iusto sint minima?"
