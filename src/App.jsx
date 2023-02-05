ee3d2cc95c1ad87decfc9f05cf397730ba70d764
Iago Alexandre <iagoaap@outlook.com>
2023-02-05 09:53:25 -0300

    init sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import { Header } from "./components/Header"
-import { Post } from "./Post"
-import "./global.css"
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import "./global.css";
+import styles from "./App.module.css";
+import { Siderbar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-      author="Diego Fernandes" 
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus cumque commodi iste cupiditate aperiam quae blanditiis id, ratione harum, delectus, hic nobis. Deleniti illo fugiat ab obcaecati iusto sint minima?"
-      />
 
-<Post 
-      author="Iago Alexandre" 
-      content="Ola, novo post"
-      />
+      <div className={styles.wrapper}>
+        <Siderbar />
+          <main>
+            <Post
+              author="Diego Fernandes"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus cumque commodi iste cupiditate aperiam quae blanditiis id, ratione harum, delectus, hic nobis. Deleniti illo fugiat ab obcaecati iusto sint minima?"
+            />
+
+            <Post
+              author="Iago Alexandre"
+              content="Ola, novo post"
+            />
+          </main>
+        
+      </div>
 
     </div>
   )
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import styles from "./Siderbar.module.css";
+
+export function Siderbar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+      className={styles.cover}
+      src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"/>
+
+      <div className={styles.profile}>
+        <strong>Iago Alexandre</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
