6c4e005ad18eaa22738f819eaf8df08ef7f2a44d
Iago Alexandre <iagoaap@outlook.com>
2023-02-05 10:31:01 -0300

    post structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post";
+import { Post } from "./components/Post";
 import "./global.css";
 import styles from "./App.module.css";
 import { Siderbar } from "./components/Sidebar";
@@ -15,11 +15,6 @@ export function App() {
             <Post
               author="Diego Fernandes"
               content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus cumque commodi iste cupiditate aperiam quae blanditiis id, ratione harum, delectus, hic nobis. Deleniti illo fugiat ab obcaecati iusto sint minima?"
-            />
-
-            <Post
-              author="Iago Alexandre"
-              content="Ola, novo post"
             />
           </main>
         
