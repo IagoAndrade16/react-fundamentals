4e89df8ec485ac8d3a1af79e91a0ee6eb75fb90a
Iago Alexandre <iagoaap@outlook.com>
2023-02-12 08:50:47 -0300

    avatar components

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,10 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 export function Comment () {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/IagoAndrade16.png" alt="" />
+      <Avatar hasBorder={false} src="https://github.com/IagoAndrade16.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
